e05cbf986950a734c099e68aed3478c398c20f0b
athlkrshna <athulkrishna1999@gmail.com>
2021-06-10 19:50:30 +0530

    images

--- src/components/pages/homepage/data.js
+++ src/components/pages/homepage/data.js
@@ -18,8 +18,8 @@ export const homeObjOne = {
     headline: 'Our Business',
     description:
       'We are a talented group of engineers coming from various industries and academia. We all have advanced degrees and numerous years of experience in the field of Artificial Intelligence. Our work has been published at numerous conferences and journals around the globe. What unites us and acted as the catalyst to form Aarna Analytics is our passion for this subject and our desire to use our expertise to create something that will help people make better decisions. We will strive to bring solutions to customers in various industries to help them make tactical and strategic decisions to overcome the challenges they face. At Aarna Analytics, we believe that our technological edge and expertise can lead our customers towards a successful future. We will work to keep our technology ahead of the curve and accessible to everyone. ',
-    imgStart: '',
-    img: 'images/image1.jpg',
+    imgStart: 'start',
+    img: 'images/image5.jpg',
     alt: 'Vault'
   };
   
@@ -30,8 +30,8 @@ export const homeObjOne = {
     headline: 'Our Technology',
     description:
       "At Aarna Analytics, we have undertaken the greatest challenge there is. Predicting the future is a tough task, even with today's technology. The Profitis Prediction Engine will use state of the art methodologies to hyper-tune our models using AI to give the best insights possible for a given problem. Our engine will analyze vast amounts of data and output usable and measurable insights that will enable our customers to reach new heights. Profitis will be a game changer!",
-    imgStart: 'start',
-    img: 'images/image1.jpg',
+    imgStart: '',
+    img: 'images/image3.jpg',
     alt: 'Vault'
   };
   
@@ -42,7 +42,7 @@ export const homeObjOne = {
     headline: 'Our Vision',
     description:
       'We are building an investment advisor portal powered by Artificial Intelligence for personal users. Thereafter, we will offer API licenses to our proprietary model for commercial customers. Providing predictive analytics services for other domains will be part of the subsequent phase. If you are interested in our product, please contact us. Stay tuned for the latest updates!',
-    imgStart: '',
-    img: 'images/image1.jpg',
+    imgStart: 'start',
+    img: 'images/image4.jpg',
     alt: 'Vault'
   };
